5a732ee2560835482f0f95bc2d225baa4c5c3bbf
Hydr0CLoric <1032055@student.sae.edu.au>
2024-02-29 23:55:24 +0800

    YOOOOOOOOOOOOO
    - FINALLY, We Have Rotation!!!!!!
    - and movement, BUT ROTATION!!!

--- Assets/DefaultAssets/Scripts/BasicEnemy.cs
+++ Assets/DefaultAssets/Scripts/BasicEnemy.cs
@@ -10,22 +10,24 @@ public class BasicEnemy : MonoBehaviour
     
     private Rigidbody2D enemyRb;
 
-    private Vector2 targetPos = Vector2.zero;
-        
+    private Vector2 move = new Vector2(1, 1);
 
     void Start()
     {
+        // null reference check, it's well used cos instantiation did not like grabbing the right object
         if(playerObj == null)
         {
-            //Debug.Log("Please attach the player object"); dw, i got it >:))
+            //Debug.Log("Please attach the player object"); dw, i got it >:)) - Need to note in user manual it's using the tag
 
             playerObj = GameObject.FindGameObjectWithTag("Player");
         }
 
+        // grab the rigid body
         enemyRb = GetComponent<Rigidbody2D>();
 
         if (enemyRb == null)
         {
+            //this error works but if i ever see it, something's wrong with the prefab (i put the rb on the prefab)
             Debug.Log("Please attach a rigid body to the enemy");
         }
     }
@@ -35,28 +37,42 @@ public class BasicEnemy : MonoBehaviour
         //check if its dead
         if(enHealth <= 0)
         {
-            //EnemyDies(); 
+            //if the enemy is dead, it will die
+            EnemyDies(); 
         }
 
-        //Use Math A-Tan and Angle Axis
-        //
-        //Vector2 relative = transform.InverseTransformPoint(targetPos);
-        //float angle = Mathf.Atan2(relative.y, relative.x);
-        //transform.rotation = Quaternion.Euler(0f, 0f, angle * Mathf.Rad2Deg);
+        /* pseudocode from Frank (i was struggling and he is awesome)
 
-        //transform.LookAt(targetPos);
+        get target position
+        target position.x equals target position.x - transform position x
+        target position.y equals target position.y - transform position y
+        angle equals mathatan(targety, targetx) * mathrad2deg
+        transform rotation equals = quaternion.euler(0,0,angle)
+        */
+        
+        Vector3 playerPos = playerObj.transform.position;
 
-        Vector3 relativePos = playerObj.transform.position - transform.position;
+        playerPos.x -= transform.position.x;
+        playerPos.y -= transform.position.y;
 
-        // the second argument, upwards, defaults to Vector3.up
-        Quaternion rotation = Quaternion.LookRotation(relativePos, Vector3.forward);
-        transform.rotation = rotation;
+        float angle = Mathf.Atan2(playerPos.y, playerPos.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+
+        //now we gotta make em move
+        // hopefully this will not take 6hrs
+        //it did not lets goooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
+
+        enemyRb.velocity = (playerObj.transform.position - transform.position) * speed * Time.deltaTime; 
+
     }
-
+    
+    //checking the collision
     protected virtual void OnCollisionEnter2D(Collision2D collision)
     {
+        //matches it to the player object
         if(collision.gameObject == playerObj)
         {
+            //Kills this enemy
             Debug.Log("Enemy hits Player");
             
             enHealth = 0;
@@ -71,36 +87,4 @@ public class BasicEnemy : MonoBehaviour
         //bye bye
         Destroy(this.gameObject);
     }
-    /* 
-    
-    if (playerObj != null)
-        {
-            // Get the direction from this object to the target object
-            Vector3 direction = playerObj.transform.position - transform.position;
-
-            // Calculate the angle from the current direction to the target direction
-            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-
-            // Apply the rotation to make this object look at the target
-            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-        }
-
-
-    //Get the player's position
-    Vector3 playerPos = new Vector3(playerObj.transform.position.x, playerObj.transform.position.y, playerObj.transform.position.z);
-    //testY = playerObj.transform.position.y;
-
-    //Vector3 vectorTest = new Vector3(0, 0, testZ);
-    //vectorTest.x = transform.position.x;
-    //vectorTest.y = transform.position.y;
-    //vectorTest.y = transform.position.y;
-
-    transform.LookAt(playerPos);
-    Vector3 newEnemyRot = new Vector3(enemyRotXY.x, enemyRotXY.y, transform.rotation.z);
-    transform.rotation = Quaternion.Euler(newEnemyRot);
-    //Debug.Log("Go to " + targetPos); // This worked :))
-
-    //enemyRb.velocity = targetPos * speed * Time.deltaTime;
-    */
 }
-
